2c09928c37e91d082fb35d8dc1ee3352abe464ee
sureshpvx <sureshsharma04885@gmail.com>
2025-05-05 12:02:17 +0530

    Prepare app for Render deployment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,6 +9,9 @@ Rails.application.configure do
   # Eager load code on boot for better performance and memory savings (ignored by Rake tasks).
   config.eager_load = true
 
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present? || ENV['RENDER'].present?
+
+
   # Full error reports are disabled.
   config.consider_all_requests_local = false
 
--- config/puma.rb
+++ config/puma.rb
@@ -39,3 +39,6 @@ plugin :solid_queue if ENV["SOLID_QUEUE_IN_PUMA"]
 # Specify the PID file. Defaults to tmp/pids/server.pid in development.
 # In other environments, only set the PID file if requested.
 pidfile ENV["PIDFILE"] if ENV["PIDFILE"]
+
+workers ENV.fetch("WEB_CONCURRENCY") { 4 }
+preload_app!
